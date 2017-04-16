db74e3e24f5d7d7d91def5263246f94d59a51f44
Anthony Dickson <eight0153@gmail.com>
2017-04-17 11:57:13 +1200

    Initial commit

---
+++ src/week09/OverhandShuffler.java
@@ -0,0 +1,32 @@
+package week09;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Implements the Overhand interface for manipulating a deck of cards using
+ * overhand shuffling.
+ * <br />
+ * Created on 17/04/2017.
+ * @author Anthony
+ */
+public class OverhandShuffler implements Overhand {
+    public void makeNew(int size) {
+        throw new NotImplementedException();
+    }
+
+    public int[] getCurrent() {
+        throw new NotImplementedException();
+    }
+
+    public void shuffle(int[] blocks) {
+        throw new NotImplementedException();
+    }
+
+    public int order(int[] blocks) {
+        throw new NotImplementedException();
+    }
+
+    public int unbrokenPairs() {
+        throw new NotImplementedException();
+    }
+}
